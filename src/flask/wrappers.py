3b90d8877a51e3a5a4c431be5f5a6c166e25a497
Markus Brandt Højgaard <118807770+Maquidos@users.noreply.github.com>
2025-03-31 10:38:00 +0200

    Add incoming files, replicated commit without comments.

--- src/flask/wrappers.py
+++ src/flask/wrappers.py
@@ -11,7 +11,7 @@ from . import json
 from .globals import current_app
 from .helpers import _split_blueprint_path
 
-if t.TYPE_CHECKING:  # pragma: no cover
+if t.TYPE_CHECKING:  
     from werkzeug.routing import Rule
 
 
@@ -30,35 +30,134 @@ class Request(RequestBase):
 
     json_module: t.Any = json
 
-    #: The internal URL rule that matched the request.  This can be
-    #: useful to inspect which methods are allowed for the URL from
-    #: a before/after handler (``request.url_rule.methods``) etc.
-    #: Though if the request's method was invalid for the URL rule,
-    #: the valid list is available in ``routing_exception.valid_methods``
-    #: instead (an attribute of the Werkzeug exception
-    #: :exc:`~werkzeug.exceptions.MethodNotAllowed`)
-    #: because the request was never internally bound.
-    #:
-    #: .. versionadded:: 0.6
+  #: The internal URL rule that matched the request.  This can be
+  #: useful to inspect which methods are allowed for the URL from
+  #: a before/after handler (``request.url_rule.methods``) etc.
+  #: Though if the request's method was invalid for the URL rule,
+  #: the valid list is available in ``routing_exception.valid_methods``
+  #: instead (an attribute of the Werkzeug exception
+  #: :exc:`~werkzeug.exceptions.MethodNotAllowed`)
+  #: because the request was never internally bound.
+  #:
+    
+  #: .. versionadded:: 0.6
+    
+    
+    
+    
+    
+    
+    
+    
+    
     url_rule: Rule | None = None
 
-    #: A dict of view arguments that matched the request.  If an exception
-    #: happened when matching, this will be ``None``.
+  #: A dict of view arguments that matched the request.  If an exception
+  #: happened when matching, this will be ``None``.
+    
+    
     view_args: dict[str, t.Any] | None = None
 
-    #: If matching the URL failed, this is the exception that will be
-    #: raised / was raised as part of the request handling.  This is
-    #: usually a :exc:`~werkzeug.exceptions.NotFound` exception or
-    #: something similar.
+  #: If matching the URL failed, this is the exception that will be
+  #: raised / was raised as part of the request handling.  This is
+  #: usually a :exc:`~werkzeug.exceptions.NotFound` exception or
+  #: something similar.
+    
+    
+    
+    
     routing_exception: HTTPException | None = None
 
+    _max_content_length: int | None = None
+    _max_form_memory_size: int | None = None
+    _max_form_parts: int | None = None
+
     @property
-    def max_content_length(self) -> int | None:  # type: ignore[override]
-        """Read-only view of the ``MAX_CONTENT_LENGTH`` config key."""
-        if current_app:
-            return current_app.config["MAX_CONTENT_LENGTH"]  # type: ignore[no-any-return]
-        else:
-            return None
+    def max_content_length(self) -> int | None:
+        """The maximum number of bytes that will be read during this request. If
+        this limit is exceeded, a 413 :exc:`~werkzeug.exceptions.RequestEntityTooLarge`
+        error is raised. If it is set to ``None``, no limit is enforced at the
+        Flask application level. However, if it is ``None`` and the request has
+        no ``Content-Length`` header and the WSGI server does not indicate that
+        it terminates the stream, then no data is read to avoid an infinite
+        stream.
+
+        Each request defaults to the :data:`MAX_CONTENT_LENGTH` config, which
+        defaults to ``None``. It can be set on a specific ``request`` to apply
+        the limit to that specific view. This should be set appropriately based
+        on an application's or view's specific needs.
+
+        .. versionchanged:: 3.1
+            This can be set per-request.
+
+        .. versionchanged:: 0.6
+            This is configurable through Flask config.
+        """
+        if self._max_content_length is not None:
+            return self._max_content_length
+
+        if not current_app:
+            return super().max_content_length
+
+        return current_app.config["MAX_CONTENT_LENGTH"]  
+
+    @max_content_length.setter
+    def max_content_length(self, value: int | None) -> None:
+        self._max_content_length = value
+
+    @property
+    def max_form_memory_size(self) -> int | None:
+        """The maximum size in bytes any non-file form field may be in a
+        ``multipart/form-data`` body. If this limit is exceeded, a 413
+        :exc:`~werkzeug.exceptions.RequestEntityTooLarge` error is raised. If it
+        is set to ``None``, no limit is enforced at the Flask application level.
+
+        Each request defaults to the :data:`MAX_FORM_MEMORY_SIZE` config, which
+        defaults to ``500_000``. It can be set on a specific ``request`` to
+        apply the limit to that specific view. This should be set appropriately
+        based on an application's or view's specific needs.
+
+        .. versionchanged:: 3.1
+            This is configurable through Flask config.
+        """
+        if self._max_form_memory_size is not None:
+            return self._max_form_memory_size
+
+        if not current_app:
+            return super().max_form_memory_size
+
+        return current_app.config["MAX_FORM_MEMORY_SIZE"]  
+
+    @max_form_memory_size.setter
+    def max_form_memory_size(self, value: int | None) -> None:
+        self._max_form_memory_size = value
+
+    @property  
+    def max_form_parts(self) -> int | None:
+        """The maximum number of fields that may be present in a
+        ``multipart/form-data`` body. If this limit is exceeded, a 413
+        :exc:`~werkzeug.exceptions.RequestEntityTooLarge` error is raised. If it
+        is set to ``None``, no limit is enforced at the Flask application level.
+
+        Each request defaults to the :data:`MAX_FORM_PARTS` config, which
+        defaults to ``1_000``. It can be set on a specific ``request`` to apply
+        the limit to that specific view. This should be set appropriately based
+        on an application's or view's specific needs.
+
+        .. versionchanged:: 3.1
+            This is configurable through Flask config.
+        """
+        if self._max_form_parts is not None:
+            return self._max_form_parts
+
+        if not current_app:
+            return super().max_form_parts
+
+        return current_app.config["MAX_FORM_PARTS"]  
+
+    @max_form_parts.setter
+    def max_form_parts(self, value: int | None) -> None:
+        self._max_form_parts = value
 
     @property
     def endpoint(self) -> str | None:
@@ -71,7 +170,7 @@ class Request(RequestBase):
         reconstruct the same URL or a modified URL.
         """
         if self.url_rule is not None:
-            return self.url_rule.endpoint  # type: ignore[no-any-return]
+            return self.url_rule.endpoint  
 
         return None
 
@@ -114,8 +213,10 @@ class Request(RequestBase):
     def _load_form_data(self) -> None:
         super()._load_form_data()
 
-        # In debug mode we're replacing the files multidict with an ad-hoc
-        # subclass that raises a different error for key errors.
+      # In debug mode we're replacing the files multidict with an ad-hoc
+      # subclass that raises a different error for key errors.
+        
+        
         if (
             current_app
             and current_app.debug
@@ -129,11 +230,11 @@ class Request(RequestBase):
     def on_json_loading_failed(self, e: ValueError | None) -> t.Any:
         try:
             return super().on_json_loading_failed(e)
-        except BadRequest as e:
+        except BadRequest as ebr:
             if current_app and current_app.debug:
                 raise
 
-            raise BadRequest() from e
+            raise BadRequest() from ebr
 
 
 class Response(ResponseBase):
@@ -161,14 +262,15 @@ class Response(ResponseBase):
     autocorrect_location_header = False
 
     @property
-    def max_cookie_size(self) -> int:  # type: ignore
+    def max_cookie_size(self) -> int:  
         """Read-only view of the :data:`MAX_COOKIE_SIZE` config key.
 
         See :attr:`~werkzeug.wrappers.Response.max_cookie_size` in
         Werkzeug's docs.
         """
         if current_app:
-            return current_app.config["MAX_COOKIE_SIZE"]  # type: ignore[no-any-return]
+            return current_app.config["MAX_COOKIE_SIZE"]  
 
-        # return Werkzeug's default when not in an app context
+      # return Werkzeug's default when not in an app context
+        
         return super().max_cookie_size
