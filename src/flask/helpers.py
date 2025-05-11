1265ff5a0f7753a4a97cc7853b8de710f25acae5
Markus Brandt Højgaard <118807770+Maquidos@users.noreply.github.com>
2025-05-11 22:25:28 +0200

    Add incoming files, replicated commit without comments.

--- src/flask/helpers.py
+++ src/flask/helpers.py
@@ -49,7 +49,7 @@ def get_load_dotenv(default: bool = True) -> bool:
 
 
 def stream_with_context(
-    generator_or_function: t.Iterator[t.AnyStr] | t.Callable[..., t.Iterator[t.AnyStr]]
+    generator_or_function: t.Iterator[t.AnyStr] | t.Callable[..., t.Iterator[t.AnyStr]],
 ) -> t.Iterator[t.AnyStr]:
     """Request contexts disappear when the response is started on the server.
     This is done for efficiency reasons and to make it less likely to encounter
@@ -159,7 +159,7 @@ Internally, this function checks the number of arguments passed and calls
         return current_app.response_class()
     if len(args) == 1:
         args = args[0]
-    return current_app.make_response(args)  # type: ignore
+    return current_app.make_response(args)
 
 
 def url_for(
