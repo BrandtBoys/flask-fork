0af5515e4b60e6ea782a497c66031b388c54941f
palleBrandt <lupa@itu.dk>
2025-05-12 23:45:05 +0200

    Remove all comments

--- examples/tutorial/flaskr/db.py
+++ examples/tutorial/flaskr/db.py
@@ -6,10 +6,6 @@ from flask import g
 
 
 def get_db():
-    """Connect to the application's configured database. The connection
-    is unique for each request and will be reused if this is called
-    again.
-    """
     if "db" not in g:
         g.db = sqlite3.connect(
             current_app.config["DATABASE"], detect_types=sqlite3.PARSE_DECLTYPES
@@ -20,9 +16,6 @@ def get_db():
 
 
 def close_db(e=None):
-    """If this request connected to the database, close the
-    connection.
-    """
     db = g.pop("db", None)
 
     if db is not None:
@@ -30,7 +23,6 @@ def close_db(e=None):
 
 
 def init_db():
-    """Clear existing data and create new tables."""
     db = get_db()
 
     with current_app.open_resource("schema.sql") as f:
@@ -39,14 +31,11 @@ def init_db():
 
 @click.command("init-db")
 def init_db_command():
-    """Clear existing data and create new tables."""
     init_db()
     click.echo("Initialized the database.")
 
 
 def init_app(app):
-    """Register database functions with the Flask app. This is called by
-    the application factory.
-    """
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
+
--- tests/test_config.py
+++ tests/test_config.py
@@ -247,3 +247,4 @@ def test_from_pyfile_weird_encoding(tmpdir, encoding):
     app.config.from_pyfile(str(f))
     value = app.config["TEST_VALUE"]
     assert value == "föö"
+
