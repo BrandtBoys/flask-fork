2edb205fdee908f19029453f08f337840a4ce3aa
Markus Brandt Højgaard <118807770+Maquidos@users.noreply.github.com>
2025-05-12 19:05:30 +0200

    Add incoming files, replicated commit without comments.

--- src/flask/helpers.py
+++ src/flask/helpers.py
@@ -105,7 +105,7 @@ Raises:
         return current_app.response_class()
     if len(args) == 1:
         args = args[0]
-    return current_app.make_response(args)  # type: ignore
+    return current_app.make_response(args)
 
 
 def url_for(
