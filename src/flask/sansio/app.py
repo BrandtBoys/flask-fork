483ed3ecc48651ae93cf78653252a95e32af89e3
Markus Brandt Højgaard <118807770+Maquidos@users.noreply.github.com>
2025-05-13 04:49:14 +0200

    Add incoming files, replicated commit without comments.

--- src/flask/sansio/app.py
+++ src/flask/sansio/app.py
@@ -522,7 +522,7 @@ class App(Scaffold):
             )
 
         if provide_automatic_options is None:
-            if "OPTIONS" not in methods and self.config["PROVIDE_AUTOMATIC_OPTIONS"]:
+            if "OPTIONS" not in methods:
                 provide_automatic_options = True
                 required_methods.add("OPTIONS")
             else:
