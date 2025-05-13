1a83ec9d0456decca1a6200008e0d031cd1ec2e6
Markus Brandt Højgaard <118807770+Maquidos@users.noreply.github.com>
2025-05-13 11:26:39 +0200

    Add incoming files, replicated commit without comments.

--- src/flask/__init__.py
+++ src/flask/__init__.py
@@ -1,7 +1,3 @@
-from __future__ import annotations
-
-import typing as t
-
 from . import json as json
 from .app import Flask as Flask
 from .blueprints import Blueprint as Blueprint
@@ -42,8 +38,6 @@ from .templating import stream_template_string as stream_template_string
 from .wrappers import Request as Request
 from .wrappers import Response as Response
 
-
-def __getattr__(name: str) -> t.Any:
     """
 Returns the version of Flask.
 
@@ -59,17 +53,3 @@ Returns:
 Raises:
     AttributeError: If the attribute does not exist.
 """
-    if name == "__version__":
-        import importlib.metadata
-        import warnings
-
-        warnings.warn(
-            "The '__version__' attribute is deprecated and will be removed in"
-            " Flask 3.2. Use feature detection or"
-            " 'importlib.metadata.version(\"flask\")' instead.",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        return importlib.metadata.version("flask")
-
-    raise AttributeError(name)
